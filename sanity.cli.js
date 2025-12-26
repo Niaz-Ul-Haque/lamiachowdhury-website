0da7a337037b0aa6fa06d7fe398cecc5697e36b9
Niaz <theniaz619@gmail.com>
2025-12-25 21:52:07 -0500

    Just deploy Sanity CMS

---
+++ sanity.cli.js
@@ -0,0 +1,11 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "ycpgp2zg",
+    dataset: "production",
+  },
+  deployment: {
+    appId: "chzv52qlrp3mo1ekgw836wm8",
+  },
+});
